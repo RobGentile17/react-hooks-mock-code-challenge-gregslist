97d00d28cb90397aa225d388567407758a9328e6
Rob <robert.gentile@powerhrg.com>
2021-07-20 14:29:28 -0400

    Core Deliverables

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,36 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Header from "./Header";
 import ListingsContainer from "./ListingsContainer";
 
 function App() {
+  const [listings, setListing] = useState([])
+  const [search, setSearch] = useState("")
+
+  useEffect(() => {
+    fetch("http://localhost:6001/listings")
+      .then((r) => r.json())
+      .then(setListing)
+  }, []);
+
+  function handleRemoveListing(id) {
+    const newListings = listings.filter((listing) => listing.id !== id);
+    setListing(newListings);
+  }
+
+
+  const showListings = listings.filter((listing) =>
+  listing.description.toLowerCase().includes(search.toLowerCase())
+);
+
   return (
     <div className="app">
-      <Header />
-      <ListingsContainer />
+      <Header 
+        onSearch={setSearch}
+      />
+      <ListingsContainer 
+        listings={showListings}
+        onDeleteListing={handleRemoveListing}
+      />
     </div>
   );
 }
